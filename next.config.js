f2d7d91dda7e451669b3a497b813b94f827683fe
Justin Bond <bond.justink@gmail.com>
2023-08-28 12:36:51 -0700

    add sentry

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const moduleExports = {
   images: {
     unoptimized: true
   }
 };
 
-module.exports = nextConfig;
+const sentryWebpackPluginOptions = {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  // Suppresses source map uploading logs during build
+  silent: true,
+
+  org: 'justin-bond-e8aefe1fe',
+  project: 'nextjs-portfolio'
+};
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
